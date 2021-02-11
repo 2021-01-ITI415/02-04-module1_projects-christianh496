0269d887f33c6ba0cea5af5ce171b7d1fe3e08ea
christianh496 <77816880+christianh496@users.noreply.github.com>
2021-02-10 22:35:51 -0500

    functioning prototype

--- Assets/02-Mission Demolition/Scripts/RigidbodySleep.cs
+++ Assets/02-Mission Demolition/Scripts/RigidbodySleep.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class RigidbodySleep : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
+    // Start is called before the first frame update
         Rigidbody rb = GetComponent<Rigidbody>();
         if (rb != null) rb.Sleep();
     }
-
     // Update is called once per frame
     void Update()
     {
